fad3774927d2aef71de9e0b515bf06160f9b26ef
gintgeor <43870464+gintgeor@users.noreply.github.com>
2020-10-14 13:39:10 -0700

    adding sftp to copy pkg to device, ssh to untar and run script

--- Learn/aMAZE/app/src/main/java/com/e/amaze/AddDevice.kt
+++ Learn/aMAZE/app/src/main/java/com/e/amaze/AddDevice.kt
@@ -9,6 +9,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.jcraft.jsch.ChannelExec
+import com.jcraft.jsch.ChannelSftp
 import com.jcraft.jsch.JSch
 import java.io.ByteArrayOutputStream
 import java.util.*
@@ -29,11 +30,20 @@ class AddDevice : AppCompatActivity() {
         val deviceDetails = findViewById<EditText>(R.id.deviceDetails)
         var output:String = "None"
 
+        class SftpTask : AsyncTask<Void, Void, String>() {
+            override fun doInBackground(vararg p0: Void?): String {
+                output = executeCopyCommand(userName.text.toString(), passwd.text.toString(), deviceIP.text.toString())
+                print(output)
+                return output
+            }
+        }
+
+        SftpTask().execute()
+
         class SshTask : AsyncTask<Void, Void, String>() {
             override fun doInBackground(vararg p0: Void?): String {
                 output = executeRemoteCommand(userName.text.toString(), passwd.text.toString(), deviceIP.text.toString())
                 print(output)
-                //deviceDetails.setText(output.toString())
                 return output
             }
         }
@@ -64,12 +74,45 @@ fun executeRemoteCommand(username: String,
     sshChannel.outputStream = outputStream
 
     // Execute command.
-    sshChannel.setCommand("uname -a")
+    sshChannel.setCommand("tar -xvzf /tmp/amazert.tar.gz && cd /tmp/amazert && ./install.sh")
     sshChannel.connect()
 
     // Sleep needed in order to wait long enough to get result back.
     Thread.sleep(1_000)
     sshChannel.disconnect()
+
+    session.disconnect()
+
+    return outputStream.toString()
+}
+
+fun executeCopyCommand(username: String,
+                         password: String,
+                         hostname: String,
+                         port: Int = 22): String {
+    val jsch = JSch()
+    val session = jsch.getSession(username, hostname, port)
+    session.setPassword(password)
+
+    // Avoid asking for key confirmation.
+    val properties = Properties()
+    properties.put("StrictHostKeyChecking", "no")
+    session.setConfig(properties)
+
+    session.connect()
+
+    // Create SSH Channel.
+    val sftpChannel = session.openChannel("sftp") as ChannelSftp
+    val outputStream = ByteArrayOutputStream()
+    sftpChannel.outputStream = outputStream
+
+    sftpChannel.connect()
+    //sftpChannel.put("I:/demo/myOutFile.txt", "/tmp/myOutFile.zip", 1 )
+    sftpChannel.put(R.raw.amazert.toString(), "/tmp/amazert.tar.gz", 1 )
+
+    // Sleep needed in order to wait long enough to get result back.
+    Thread.sleep(1_000)
+    sftpChannel.disconnect()
 
     session.disconnect()
 
