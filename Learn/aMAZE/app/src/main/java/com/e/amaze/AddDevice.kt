bd9cb74435c8e961042e63b5cb0cc4e79c15470c
gintgeor <43870464+gintgeor@users.noreply.github.com>
2020-10-25 14:20:35 -0700

    corrected pkg location to asset dir, ssh init for installing sftp server, correcting sftp input file stream

--- Learn/aMAZE/app/src/main/java/com/e/amaze/AddDevice.kt
+++ Learn/aMAZE/app/src/main/java/com/e/amaze/AddDevice.kt
@@ -1,18 +1,18 @@
 package com.e.amaze
 
-import android.graphics.Color
+import android.content.res.AssetManager
 import android.os.AsyncTask
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.jcraft.jsch.ChannelExec
 import com.jcraft.jsch.ChannelSftp
+import com.jcraft.jsch.ChannelSftp.OVERWRITE
 import com.jcraft.jsch.JSch
-import java.io.ByteArrayOutputStream
+import java.io.*
 import java.util.*
+
 
 class AddDevice : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,27 +38,84 @@ class AddDevice : AppCompatActivity() {
             }
 
             override fun onPostExecute(result: String) {
-                deviceDetails.setText(output.toString())
+                //deviceDetails.setText(output.toString())
+                deviceDetails.setText("ssh done")
+                //deviceDetails.setText(Uri.parse("android.resource://com.e.amaze/" + R.raw.amazert).toString())
             }
         }
 
         class SftpTask : AsyncTask<Void, Void, String>() {
             override fun doInBackground(vararg p0: Void?): String {
-                output = executeCopyCommand(userName.text.toString(), passwd.text.toString(), deviceIP.text.toString())
+                var am: AssetManager = getAssets()
+                var inputStream: InputStream = am.open("amazert.pkg")
+
+                output = executeCopyCommand(userName.text.toString(), passwd.text.toString(), deviceIP.text.toString(), 22, inputStream)
                 print(output)
                 return output
             }
 
             override fun onPostExecute(result: String) {
-                deviceDetails.setText(output.toString())
+                //deviceDetails.setText(output.toString())
+                deviceDetails.setText("Sftp Done, start Ssh ...")
+                print("Sftp Done, start Ssh ...")
                 SshTask().execute()
             }
         }
 
-        SftpTask().execute()
+        class SshInitTask : AsyncTask<Void, Void, String>() {
+            override fun doInBackground(vararg p0: Void?): String {
+                output = executeRemoteInitCommand(userName.text.toString(), passwd.text.toString(), deviceIP.text.toString())
+                print(output)
+                return output
+            }
+
+            override fun onPostExecute(result: String) {
+                deviceDetails.setText("Ssh Init Done, start sftp ...")
+                print("Ssh Init Done, start sftp ...")
+                SftpTask().execute()
+            }
+        }
+
+        //SftpTask().execute()
+        //SshTask().execute()
+        SshInitTask().execute()
 
     }
 
+}
+
+fun executeRemoteInitCommand(username: String,
+                         password: String,
+                         hostname: String,
+                         port: Int = 22): String {
+    val jsch = JSch()
+    val session = jsch.getSession(username, hostname, port)
+    session.setPassword(password)
+
+    // Avoid asking for key confirmation.
+    val properties = Properties()
+    properties.put("StrictHostKeyChecking", "no")
+    session.setConfig(properties)
+
+    session.connect()
+
+    // Create SSH Channel.
+    val sshChannel = session.openChannel("exec") as ChannelExec
+    val outputStream = ByteArrayOutputStream()
+    sshChannel.outputStream = outputStream
+
+    // Execute command.
+    sshChannel.setCommand("opkg update && opkg install openssh-sftp-server")
+    //sshChannel.setCommand("uname -a")
+    sshChannel.connect()
+
+    // Sleep needed in order to wait long enough to get result back.
+    Thread.sleep(1_000)
+    sshChannel.disconnect()
+
+    session.disconnect()
+
+    return outputStream.toString()
 }
 
 fun executeRemoteCommand(username: String,
@@ -81,8 +138,10 @@ fun executeRemoteCommand(username: String,
     val outputStream = ByteArrayOutputStream()
     sshChannel.outputStream = outputStream
 
+    sshChannel.connect(10000)
     // Execute command.
-    sshChannel.setCommand("tar -xvzf /tmp/amazert.tar.gz && cd /tmp/amazert && ./install.sh")
+    sshChannel.setCommand("cd /tmp && tar -xvzf amazert.tar.gz && cd /tmp/amazert && ./install.sh")
+    //sshChannel.setCommand("uname -a")
     sshChannel.connect()
 
     // Sleep needed in order to wait long enough to get result back.
@@ -97,7 +156,7 @@ fun executeRemoteCommand(username: String,
 fun executeCopyCommand(username: String,
                          password: String,
                          hostname: String,
-                         port: Int = 22): String {
+                         port: Int = 22, inputStream: InputStream): String {
     val jsch = JSch()
     val session = jsch.getSession(username, hostname, port)
     session.setPassword(password)
@@ -111,12 +170,12 @@ fun executeCopyCommand(username: String,
 
     // Create SSH Channel.
     val sftpChannel = session.openChannel("sftp") as ChannelSftp
-    val outputStream = ByteArrayOutputStream()
-    sftpChannel.outputStream = outputStream
+    //val outputStream = ByteArrayOutputStream()
+    //sftpChannel.outputStream = outputStream
 
-    sftpChannel.connect()
+    sftpChannel.connect(10000)
     //sftpChannel.put("I:/demo/myOutFile.txt", "/tmp/myOutFile.zip", 1 )
-    sftpChannel.put(R.raw.amazert.toString(), "/tmp/amazert.tar.gz", 1 )
+    sftpChannel.put(inputStream, "/tmp/amazert.tar.gz", OVERWRITE)
 
     // Sleep needed in order to wait long enough to get result back.
     Thread.sleep(1_000)
@@ -124,5 +183,6 @@ fun executeCopyCommand(username: String,
 
     session.disconnect()
 
-    return outputStream.toString()
+    //return outputStream.toString()
+    return "Sftp Done"
 }
