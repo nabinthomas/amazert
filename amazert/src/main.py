9369de668ad4cb592d000a6f48c1cffeb6f1b3f7
nabinthomas <nabin.thomas@gmail,com>
2020-10-25 20:30:14 -0700

    Added identifier to all messages sent from SW to cloud, Added documentation for fucntions in main.py

--- amazert/src/main.py
+++ amazert/src/main.py
@@ -14,20 +14,64 @@ import subprocess
 
 keepRunning = True    
 
+"""
+Prepares a packet to send. 
+Every packet will need to be added with some extra information about the 
+identification of the device being controlled/reported. This is a wrapper for generating
+and updating that data
+
+@param config - JSON Object holding the device configuration (ID data)
+@param dataToSend - JSON Object holding the data to be sent
+
+@return - JSON Object with config data added to dataToSend
+
+"""
+def preparePacketToSend(config, dataToSend):
+    jsonPacket = dataToSend
+    jsonPacket['identifier'] = config
+    return jsonPacket
+
+"""
+Thread implementing Heartbeat support. This sends all the current settings to the Cloud server once on boot
+and then keeps sending a hearbeat message at heartBeatInterval
+"""
 class amazeRTHeartBeatThread(threading.Thread):
     def __init__(self, config, websocket):
         threading.Thread.__init__(self)
         self.websocket = websocket
         self.config = config
+
     def run(self):
         while (keepRunning == True):
             self.sendHeartbeat()
             time.sleep(10)
-    def sendHeartbeat(self):
-        message = '{ "action" : "register"}'
-        print(f"> {message}")
-        self.websocket.send(message)
 
+    def sendAllConfiguration(self):
+        message = { "action" : "register"}
+        print(f"> {message}")
+        self.websocket.send(preparePacketToSend(self.config, message))
+
+    def sendHeartbeat(self):
+        message = { "action" : "heartbeat"}
+        print(f"> {message}")
+        self.websocket.send(preparePacketToSend(self.config, message))
+
+"""
+Handles a command sent from the cloud.
+Once the processing is complete, a response is sent back to the server
+in the format like 
+                {   
+                     "action" : "response",
+                     "response" : resultString, # output of the command executed.
+                     "code" : responseCode 
+                }
+@note identifier is always added to the reply to server
+
+@param config - Identification for this device. 
+@param websocket - used for communicating with the server
+@command array of command + command line params
+@options Special options to run the command. Unused for now. Added for future enhancements
+"""
 async def amazeRTCommandHandler(config, websocket, command, options):
     print ("Executing command : > " + json.dumps(command))
     # Send a response.
@@ -47,26 +91,58 @@ async def amazeRTCommandHandler(config, websocket, command, options):
         resultString = str(e)
         responseCode = -1
     
-    responseJson = { "action" : "response",
+    responseJson = {
+                     "action" : "response",
                      "response" : resultString,
                      "code" : responseCode 
                     }
+
+    responseJson = preparePacketToSend(config, responseJson)
     await websocket.send(json.dumps(responseJson))
 
+"""
+Handle a request to apply a particular setting
+@param config - Identification for this device. 
+@param websocket - used for communicating with the server
+@setting name and value for the setting to be applied
+@options Special options to run the command. Unused for now. Added for future enhancements
+
+"""
 async def amazerRTSettingHandler(config, websocket, setting, options):
     print ("Applying setting : > " + json.dumps(setting))
 
+"""
+Handle a request to control the amazeRT SW
+@param config - Identification for this device. 
+@param websocket - used for communicating with the server
+@control control information
+@options Special options to run the command. Unused for now. Added for future enhancements
+
+"""
 async def amazerRTControlHandler(config, websocket, control, options):
     print ("Responding to control : > " + json.dumps(control))
     if control == "exit":
         keepRunning = False
 
+"""
+Used to map the action to a function that can handle that kind of action
+"""
 actionHandlerMappings = {
     "command" : amazeRTCommandHandler, 
     "setting" : amazerRTSettingHandler,
     "control"  : amazerRTControlHandler
  }
 
+"""
+Entry point for any request that comes to the amazeRT management service
+
+@note - Requests will be handled only if the identifier in the request messsage from the websocket
+matches the local configuration in config
+
+@param config - Identification for this device. 
+@param websocket - used for communicating with the server
+
+"""
 async def amazeRTActionHandler(config, websocket):
     print ("amazeRTActionHandler start ")
     async for message in websocket:
@@ -80,12 +156,16 @@ async def amazeRTActionHandler(config, websocket):
             except KeyError:
                 options = {}
             actionHandler = actionHandlerMappings[action]
-            await actionHandler(websocket, actionParams, options)
+            await actionHandler(config, websocket, actionParams, options)
         except KeyError:
-            print ("Unsupported Action")
+            print ("Unsupported Action > " )
         if (message == "exit"):
             keepRunning = False
 
+"""
+Main for the service. Connects to the Cloud App Engine, Initialize local handlers 
+and keep listening to requests
+"""
 async def amazeRTServiceMain():
     uri = "ws://localhost:6789"
     config = {}
