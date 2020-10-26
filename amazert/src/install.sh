e0753f10824e6e7fa34d07096d98a594fd8bb9c9
nabinthomas <nabin.thomas@gmail,com>
2020-10-25 19:20:48 -0700

    WAR for broken pip3 install

--- amazert/src/install.sh
+++ amazert/src/install.sh
@@ -2,4 +2,8 @@ opkg update
 opkg install openssl-util
 opkg install python3
 opkg install python3-pip
+# WAR to fix broken pip3. Forcing this for now. 
+opkg install curl
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py --force-reinstall
 pip3 install websockets
