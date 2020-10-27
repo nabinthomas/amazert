f181e18a31502c5951c9fe96df320c38686acfff
gintgeor <43870464+gintgeor@users.noreply.github.com>
2020-10-27 00:52:23 -0700

    exec scripts in device done, secure copy amaert.json

--- aMAZE/app/src/main/java/com/e/amaze/AddDevice.kt
+++ aMAZE/app/src/main/java/com/e/amaze/AddDevice.kt
@@ -10,7 +10,10 @@ import com.jcraft.jsch.ChannelExec
 import com.jcraft.jsch.ChannelSftp
 import com.jcraft.jsch.ChannelSftp.OVERWRITE
 import com.jcraft.jsch.JSch
-import java.io.*
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
 import java.util.*
 
 
@@ -30,6 +33,20 @@ class AddDevice : AppCompatActivity() {
         val deviceDetails = findViewById<EditText>(R.id.deviceDetails)
         var output:String = "None"
 
+        class SftpGetTask : AsyncTask<Void, Void, String>() {
+            override fun doInBackground(vararg p0: Void?): String {
+
+                output = executeGetCommand(userName.text.toString(), passwd.text.toString(), deviceIP.text.toString(), 22)
+                print(output)
+                return output
+            }
+
+            override fun onPostExecute(result: String) {
+                //deviceDetails.setText(output.toString())
+                deviceDetails.setText("Sftp Get Done")
+                print("Sftp Get Done")
+            }
+        }
         class SshTask : AsyncTask<Void, Void, String>() {
             override fun doInBackground(vararg p0: Void?): String {
                 output = executeRemoteCommand(userName.text.toString(), passwd.text.toString(), deviceIP.text.toString())
@@ -41,6 +58,7 @@ class AddDevice : AppCompatActivity() {
                 //deviceDetails.setText(output.toString())
                 deviceDetails.setText("ssh done")
                 //deviceDetails.setText(Uri.parse("android.resource://com.e.amaze/" + R.raw.amazert).toString())
+                SftpGetTask().execute()
             }
         }
 
@@ -135,17 +153,20 @@ fun executeRemoteCommand(username: String,
 
     // Create SSH Channel.
     val sshChannel = session.openChannel("exec") as ChannelExec
+    //val sshChannel = session.openChannel("shell") as ChannelShell
     val outputStream = ByteArrayOutputStream()
     sshChannel.outputStream = outputStream
 
-    sshChannel.connect(10000)
+    sshChannel.connect(120000)
     // Execute command.
-    sshChannel.setCommand("cd /tmp && tar -xvzf amazert.tar.gz && cd /tmp/amazert && ./install.sh")
+    //TODO - get email id from previous context
+    val cmd = "cd /tmp && tar -xvzf amazert.tar.gz && cd /tmp/amazert && chmod 744 install.sh && ./install.sh && python3 init.py " + "ginto100@gmail.com 1234"
+    sshChannel.setCommand(cmd)
     //sshChannel.setCommand("uname -a")
     sshChannel.connect()
 
     // Sleep needed in order to wait long enough to get result back.
-    Thread.sleep(1_000)
+    Thread.sleep(30_000)
     sshChannel.disconnect()
 
     session.disconnect()
@@ -185,4 +206,51 @@ fun executeCopyCommand(username: String,
 
     //return outputStream.toString()
     return "Sftp Done"
+}
+
+fun executeGetCommand(username: String,
+                       password: String,
+                       hostname: String,
+                       port: Int = 22): String {
+    val jsch = JSch()
+    val session = jsch.getSession(username, hostname, port)
+    session.setPassword(password)
+
+    // Avoid asking for key confirmation.
+    val properties = Properties()
+    properties.put("StrictHostKeyChecking", "no")
+    session.setConfig(properties)
+
+    session.connect()
+
+    // Create SSH Channel.
+    val sftpChannel = session.openChannel("sftp") as ChannelSftp
+
+    sftpChannel.connect(10000)
+
+    val remoteFile = "/etc/amazert.json"
+
+    val iStream: InputStream = sftpChannel.get(remoteFile)
+
+    // set local file
+    //val tFile = FileOutputStream("amazert.json")
+    //val tFile = FileOutputStream(File("amazert.json"))
+
+    // read local file contents
+    //var c: Int
+    //while (iStream.read().also { c = it } != -1) {
+    //    tFile.write(c)
+    //}
+
+    iStream.close()
+    //tFile.close()
+
+    // Sleep needed in order to wait long enough to get result back.
+    Thread.sleep(1_000)
+    sftpChannel.disconnect()
+
+    session.disconnect()
+
+    //return outputStream.toString()
+    return "Sftp Get Done"
 }
