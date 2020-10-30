936fc8ac5fdc83ccf240653fd6df8203c13567a4
binukjose <binukjose@gmail.com>
2020-10-30 00:32:52 -0700

    Cloud Function race condition fixed

--- cloudFunctions/main.py
+++ cloudFunctions/main.py
@@ -3,13 +3,15 @@ import json
 uri = "ws://amaze-id1.wl.r.appspot.com/notify"
 
 from websocket import create_connection
-ws = create_connection(uri)
+
 
 def notify_cloud(message):
+    ws = create_connection(uri)
     ws.send(message)
     
 def hello_rtdb(event, context):
-    """Triggered by a change to a Firebase RTDB reference.
+    """
+    Triggered by a change to a Firebase RTDB reference.
     Args:
          event (dict): Event payload.
          context (google.cloud.functions.Context): Metadata for the event.
