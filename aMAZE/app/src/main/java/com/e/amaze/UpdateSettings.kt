a9c3cd370b8af372248dde58444f4774f4f711b6
spanakkal <sandeep.panakkal@gmail.com>
2020-11-03 17:39:52 -0800

    Updating Setting UI screen with switch controls

--- aMAZE/app/src/main/java/com/e/amaze/UpdateSettings.kt
+++ aMAZE/app/src/main/java/com/e/amaze/UpdateSettings.kt
@@ -3,9 +3,12 @@ package com.e.amaze
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.telephony.mbms.StreamingServiceInfo
+import android.util.Log
 import android.view.View
 import android.widget.Spinner
+import android.widget.Switch
 import android.widget.TextView
+import androidx.core.view.isVisible
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
@@ -15,6 +18,11 @@ class UpdateSettings : AppCompatActivity() {
     lateinit var settingName:String
     lateinit var settingValue:String
     lateinit var databaseIndex:String
+
+    lateinit var settingNameView: TextView
+    lateinit var settingValueView: TextView
+    lateinit var switchCtl: Switch
+
     private val database = Firebase.database.getReferenceFromUrl("https://amaze-id1.firebaseio.com/").database
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,16 +33,49 @@ class UpdateSettings : AppCompatActivity() {
         settingValue = intent.getStringExtra("Value")
         databaseIndex = intent.getStringExtra("dbIndex")
 
-        val settingNameView: TextView = findViewById(R.id.textViewUpdateName)
+        settingNameView = findViewById(R.id.textViewUpdateName)
         settingNameView.setText(settingName.toString())
-        val settingValueView: TextView = findViewById(R.id.EditTextUpdateValue)
+        settingValueView = findViewById(R.id.EditTextUpdateValue)
         settingValueView.setText(settingValue.toString())
+        switchCtl = findViewById(R.id.switch1)
 
+        updateUIControls()
+    }
+
+    fun updateUIControls() {
+        when(settingName){
+            "wireless.wifinet0.disabled" -> {
+                switchCtl.visibility = View.VISIBLE
+                switchCtl.isChecked = settingValue.toString() != "1"
+                settingValueView.visibility = View.INVISIBLE
+            }
+            else -> {
+                Log.d("ERROR", "Invalid Key")
+                switchCtl.visibility = View.INVISIBLE
+                settingValueView.visibility = View.VISIBLE
+            }
+        }
+
+    }
+
+    fun getUpdateVal():String {
+        var updateVal:String = ""
+        if (switchCtl.visibility === View.VISIBLE){
+            if(switchCtl.isChecked) {
+                updateVal = "0"
+            }else{
+                updateVal = "1"
+            }
+        }else {
+            updateVal = settingValueView.text.toString()
+        }
+        return updateVal
     }
 
     fun updateSetting(view: View) {
         val settingValueView: TextView = findViewById(R.id.EditTextUpdateValue)
-        val updateVal = settingValueView.text
+        //val updateVal = settingValueView.text
+        val updateVal = getUpdateVal()
 
         val userId = FirebaseAuth.getInstance().currentUser?.uid
         var deviceId: String = MyApplication.Companion.register.deviceId
